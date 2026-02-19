d1fe50265533b885f9a48e191c8e390be98cdd1c
mdpos95-tech <mdpos95@gmail.com>
2026-02-19 17:15:45 +0000

    somehow forgot html tags in html file. added

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
 const startBtn = document.getElementById('quiz-start')
 if
 (!startBtn) {
-   console.warn('Start Quiz button not found. Check the HTML for an element with id "quiz-start".'); return;}
+   console.warn('Start quiz button not found. Check the HTML for an element with id "quiz-start".'); return;}
 
 startBtn.addEventListener('click', (event) => {
     event.preventDefault();
