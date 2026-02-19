ca7a660b7487d79565a356b190edebb508ac3276
mdpos95-tech <mdpos95@gmail.com>
2026-02-19 16:43:42 +0000

    adding click event to quiz button using JS

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -3,3 +3,5 @@ const startBtn = document.getElementById('quiz-start')
 if
 (!startBtn) {
    console.warn('Start Quiz button not found. Check the HTML for an element with id "quiz-start".'); return;}
+
+startBtn.addEventListener('click', (event) => {
