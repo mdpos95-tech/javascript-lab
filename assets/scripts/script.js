375395dcd1b34a674537aa6fa2a1225741f93e53
mdpos95-tech <mdpos95@gmail.com>
2026-02-19 16:51:00 +0000

    closing bracket for event and wrapper

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -7,4 +7,4 @@ if
 startBtn.addEventListener('click', (event) => {
     event.preventDefault();
     console.log('Start Quiz button clicked!');
-    alert('Quiz starting...');
+    alert('Quiz starting...');  }); });
